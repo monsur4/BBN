b3ff9b4943dcae8233e62745383edfe5ab31140d
Monsur <okuniyimonsuru@yahoo.com>
2021-10-03 17:40:42 +0100

    -update all contestants information B

--- app/src/main/java/com/mon/bbn/data/DataManager.kt
+++ app/src/main/java/com/mon/bbn/data/DataManager.kt
@@ -21,28 +21,114 @@ object DataManager {
         initWinnersOfHOH()
     }
 
+    //TODO
     // consider having all contestant details as a constant val
     private fun initializeContestants(){
         // add a few static contestants to the list
         // this would be changed later to use ROOM
-        var contestant = Contestant("Boma", 25, Sex.Male(), Popularity.AnyNumber(60), 40,
-            Status.Active(), loremIpsumText, false, tag = "boma", "Rivers, Nigeria")
+        var contestant = Contestant("Boma", 34, Sex.Male(), Popularity.AnyNumber(60), 40,
+            Status.Active(), loremIpsumText, false, tag = Contestant.BOMA, "Rivers, Nigeria")
         contestants.add(contestant)
 
-        contestant = Contestant("Saskay", 23, Sex.Female(),  Popularity.AnyNumber(66), 50,
-            Status.Eliminated(), loremIpsumText, true, tag = "saskay", "Adamawa, Nigeria")
+        contestant = Contestant("Saskay", 22, Sex.Female(),  Popularity.AnyNumber(66), 50,
+            Status.Eliminated(), loremIpsumText, true, tag = Contestant.SASKAY, "Adamawa, Nigeria")
         contestants.add(contestant)
 
-        contestant = Contestant("WhiteMoney", 28, Sex.Male(),Popularity.AnyNumber(80), 70,
-            Status.Active(), loremIpsumText, true, tag = "white_money", "Enugu, Nigeria")
+        contestant = Contestant("WhiteMoney", 29, Sex.Male(),Popularity.AnyNumber(80), 70,
+            Status.Active(), loremIpsumText, true, tag = Contestant.WHITE_MONEY, "Enugu, Nigeria")
         contestants.add(contestant)
 
-        contestant = Contestant("Nini", 25, Sex.Female(), Popularity.AnyNumber(88), 76,
-            Status.Active(), loremIpsumText, false,tag = "nini", "Edo, Nigeria")
+        contestant = Contestant("Nini", 27, Sex.Female(), Popularity.AnyNumber(88), 76,
+            Status.Active(), loremIpsumText, false,tag = Contestant.NINI, "Edo, Nigeria")
         contestants.add(contestant)
 
-        contestant = Contestant("Angel", 24, Sex.Female(), Popularity.AnyNumber(90), 56,
-            Status.Active(), loremIpsumText, true, tag = "angel", "Akwa Ibom, Nigeria")
+        contestant = Contestant("Angel", 21, Sex.Female(), Popularity.AnyNumber(90), 56,
+            Status.Active(), loremIpsumText, true, tag = Contestant.ANGEL, "Akwa Ibom, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Cross", 30, Sex.Male(), Popularity.AnyNumber(90), 56,
+            Status.Eliminated(), loremIpsumText, true, tag = Contestant.CROSS, "Anambra, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Emmanuel", 24, Sex.Male(), Popularity.AnyNumber(60), 52,
+            Status.Eliminated(), loremIpsumText, false, tag = Contestant.EMMANUEL, "Akwa Ibom, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Liquorose", 26, Sex.Female(), Popularity.AnyNumber(76), 72,
+            Status.Active(), loremIpsumText, true, tag = Contestant.LIQUOROSE, "Edo, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Saga", 28, Sex.Male(), Popularity.AnyNumber(86), 78,
+            Status.Active(), loremIpsumText, true, tag = Contestant.SAGA, "Lagos, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Maria", 29, Sex.Female(), Popularity.AnyNumber(64), 63,
+            Status.Eliminated(), "A former Air Hostess", true, tag = Contestant.MARIA, "Imo, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Peace", 26, Sex.Female(), Popularity.AnyNumber(54), 78,
+            Status.Eliminated(), loremIpsumText, false, tag = Contestant.PEACE, "Rivers, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Jay Paul", 29, Sex.Male(), Popularity.AnyNumber(70), 67,
+            Status.Eliminated(), loremIpsumText, false, tag = Contestant.JAY_PAUL, "Cross River, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Niyi", 33, Sex.Male(), Popularity.AnyNumber(67), 45,
+            Status.Eliminated(), "A Computer Engineer who loves basketball", true, tag = Contestant.NIYI, "Oyo, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Arin", 29, Sex.Female(), Popularity.AnyNumber(68), 57,
+            Status.Eliminated(), loremIpsumText, true, tag = Contestant.ARIN, "Ekiti, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Beatrice", 28, Sex.Female(), Popularity.AnyNumber(70), 76,
+            Status.Active(), "A talented software developer", false, tag = Contestant.BEATRICE, "Rivers, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Pere", 35, Sex.Male(), Popularity.AnyNumber(82), 60,
+            Status.Active(), loremIpsumText, true, tag = Contestant.PERE, "---, Nigeria")
+        contestants.add(contestant)
+
+
+        contestant = Contestant("Yousef", 29, Sex.Male(), Popularity.AnyNumber(75), 80,
+            Status.Active(), "A teacher and graduate of University of Jos", true, tag = Contestant.YOUSEF, "Nasarawa, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Jackie B", 29, Sex.Female(), Popularity.AnyNumber(80), 82,
+            Status.Active(), loremIpsumText, true, tag = Contestant.JACKIE_B, "Adamawa, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Sammie", 26, Sex.Male(), Popularity.AnyNumber(67), 50,
+            Status.Eliminated(), loremIpsumText, false, tag = Contestant.SAMMIE, "Kaduna, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Princess", 30, Sex.Female(), Popularity.AnyNumber(89), 84,
+            Status.Eliminated(), loremIpsumText, true, tag = Contestant.PRINCESS, "Imo, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Tega", 29, Sex.Female(), Popularity.AnyNumber(79), 60,
+            Status.Eliminated(), loremIpsumText, false, tag = Contestant.TEGA, "Cross River, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Micheal", 29, Sex.Male(), Popularity.AnyNumber(52), 90,
+            Status.Eliminated(), loremIpsumText, true, tag = Contestant.MICHEAL, "---, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("JMK", 23, Sex.Female(), Popularity.AnyNumber(76), 83,
+            Status.Active(), "A law graduate of UNILORIN", true, tag = Contestant.JMK, "Kwara, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Kayvee", 26, Sex.Male(), Popularity.AnyNumber(89), 34,
+            Status.Eliminated(), "First Housemate to leave the house", false, tag = Contestant.KAYVEE, "Ogun, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Yerins", 21, Sex.Male(), Popularity.AnyNumber(70), 75,
+            Status.Eliminated(), "A medical doctor", true, tag = Contestant.SAMMIE, "---, Nigeria")
+        contestants.add(contestant)
+
+        contestant = Contestant("Queen", 26, Sex.Female(), Popularity.AnyNumber(73), 65,
+            Status.Active(), loremIpsumText, false, tag = Contestant.QUEEN, "Akwa Ibom, Nigeria")
         contestants.add(contestant)
     }
 
@@ -59,13 +145,35 @@ object DataManager {
         seasons.add(season)
     }
 
+    //TODO
     private fun initImageDrawables(){
         // hashmap of image references as ints to identify each imageview unique for transition
-        images.put("angel", R.drawable.angel_circle)
-        images.put("boma", R.drawable.boma_circle)
-        images.put("nini", R.drawable.nini_circle)
-        images.put("saskay", R.drawable.saskay_circle)
-        images.put("white_money", R.drawable.whitemoney_circle)
+        images.put(Contestant.ANGEL, R.drawable.angel_circle)
+        images.put(Contestant.BOMA, R.drawable.boma_circle)
+        images.put(Contestant.NINI, R.drawable.nini_circle)
+        images.put(Contestant.SASKAY, R.drawable.saskay_circle)
+        images.put(Contestant.WHITE_MONEY, R.drawable.whitemoney_circle)
+        images.put(Contestant.CROSS, R.drawable.cross_profile)
+        images.put(Contestant.NIYI, R.drawable.niyi_profile)
+        images.put(Contestant.SAGA, R.drawable.saga_profile)
+        images.put(Contestant.SAMMIE, R.drawable.sammie_profile)
+        images.put(Contestant.YERINS, R.drawable.yerins_profile)
+        images.put(Contestant.YOUSEF, R.drawable.yousef_profile)
+        images.put(Contestant.ARIN, R.drawable.arin_profile)
+        images.put(Contestant.BEATRICE, R.drawable.beatrice_profile)
+        images.put(Contestant.LIQUOROSE, R.drawable.liqourose_profile)
+        images.put(Contestant.PRINCESS, R.drawable.princess_profile)
+        images.put(Contestant.TEGA, R.drawable.tega_profile)
+        images.put(Contestant.MICHEAL, R.drawable.micheal_profile)
+        images.put(Contestant.JMK, R.drawable.jmk_profile)
+        images.put(Contestant.KAYVEE, R.drawable.kayvee_profile)
+        images.put(Contestant.QUEEN, R.drawable.queen_profile)
+        images.put(Contestant.PEACE, R.drawable.peace_profile)
+        images.put(Contestant.PERE, R.drawable.pere_profile)
+        images.put(Contestant.MARIA, R.drawable.maria_profile)
+        images.put(Contestant.JAY_PAUL, R.drawable.jaypaul_profile)
+        images.put(Contestant.JACKIE_B, R.drawable.jackieb_profile)
+        images.put(Contestant.EMMANUEL, R.drawable.emmanuel_profile)
     }
 
     private fun initWinnersOfHOH(){
@@ -102,8 +210,8 @@ object DataManager {
 
         // week 6
         listOfWinners = ArrayList<HOHWinner>()
-        listOfWinners.add(HOHWinner("JayPaul", 25, Contestant.JAY_PAUL))
-        listOfWinners.add(HOHWinner("JackieB", 25, Contestant.JACKIE_B))
+        listOfWinners.add(HOHWinner("Jay Paul", 25, Contestant.JAY_PAUL))
+        listOfWinners.add(HOHWinner("Jackie B", 25, Contestant.JACKIE_B))
         weeksHOHWinner = WeeksHOHWinner(6, 2, listOfWinners)
         winnersOfHOH.add(weeksHOHWinner)
 
--- app/src/main/java/com/mon/bbn/entity/Contestant.kt
+++ app/src/main/java/com/mon/bbn/entity/Contestant.kt
@@ -11,8 +11,23 @@ data class Contestant(var name:String, var age:Int, var sex: Sex, var popularity
     companion object{
         const val ANGEL : String = "angel"
         const val BOMA : String = "boma"
+        const val CROSS : String = "cross"
+        const val NIYI : String = "niyi"
+        const val SAGA : String = "saga"
+        const val SAMMIE : String = "sammie"
+        const val YERINS : String = "yerins"
+        const val YOUSEF : String = "yousef"
+        const val ARIN : String = "arin"
+        const val BEATRICE : String = "beatrice"
+        const val LIQUOROSE : String = "liquorose"
+        const val PRINCESS : String = "princess"
         const val NINI : String = "nini"
         const val SASKAY : String = "saskay"
+        const val TEGA : String = "tega"
+        const val MICHEAL : String = "micheal"
+        const val JMK : String = "jmk"
+        const val KAYVEE : String = "kayvee"
+        const val QUEEN : String = "queen"
         const val WHITE_MONEY : String = "white_money"
         const val PEACE : String = "peace"
         const val PERE : String = "pere"
@@ -28,6 +43,27 @@ data class Contestant(var name:String, var age:Int, var sex: Sex, var popularity
             else if(tag == NINI) return R.drawable.nini_circle
             else if(tag == SASKAY) return R.drawable.saskay_circle
             else if(tag == WHITE_MONEY) return R.drawable.whitemoney_circle
+            else if(tag == CROSS) return R.drawable.cross_profile
+            else if(tag == NIYI) return R.drawable.niyi_profile
+            else if(tag == SAGA) return R.drawable.saga_profile
+            else if(tag == SAMMIE) return R.drawable.sammie_profile
+            else if(tag == YERINS) return R.drawable.yerins_profile
+            else if(tag == YOUSEF) return R.drawable.yousef_profile
+            else if(tag == ARIN) return R.drawable.arin_profile
+            else if(tag == BEATRICE) return R.drawable.beatrice_profile
+            else if(tag == LIQUOROSE) return R.drawable.liqourose_profile
+            else if(tag == PRINCESS) return R.drawable.princess_profile
+            else if(tag == TEGA) return R.drawable.tega_profile
+            else if(tag == MICHEAL) return R.drawable.micheal_profile
+            else if(tag == JMK) return R.drawable.jmk_profile
+            else if(tag == KAYVEE) return R.drawable.kayvee_profile
+            else if(tag == QUEEN) return R.drawable.queen_profile
+            else if(tag == PEACE) return R.drawable.peace_profile
+            else if(tag == PERE) return R.drawable.pere_profile
+            else if(tag == MARIA) return R.drawable.maria_profile
+            else if(tag == JAY_PAUL) return R.drawable.jaypaul_profile
+            else if(tag == JACKIE_B) return R.drawable.jackieb_profile
+            else if(tag == EMMANUEL) return R.drawable.emmanuel_profile
             else return R.drawable.boma_circle // bomas image is our default image
         }
 
@@ -37,6 +73,27 @@ data class Contestant(var name:String, var age:Int, var sex: Sex, var popularity
             else if(tag == NINI) return R.drawable.nini_background
             else if(tag == SASKAY) return R.drawable.saskay_background
             else if(tag == WHITE_MONEY) return R.drawable.whitemoney_background
+            else if(tag == CROSS) return R.drawable.cross_background
+            else if(tag == NIYI) return R.drawable.niyi_background
+            else if(tag == SAGA) return R.drawable.saga_background
+            else if(tag == SAMMIE) return R.drawable.sammie_background
+            else if(tag == YERINS) return R.drawable.yerins_background
+            else if(tag == YOUSEF) return R.drawable.yousef_background
+            else if(tag == ARIN) return R.drawable.arin_background
+            else if(tag == BEATRICE) return R.drawable.beatrice_background
+            else if(tag == LIQUOROSE) return R.drawable.liqourose_background
+            else if(tag == PRINCESS) return R.drawable.princess_background
+            else if(tag == TEGA) return R.drawable.tega_background
+            else if(tag == MICHEAL) return R.drawable.micheal_background
+            else if(tag == JMK) return R.drawable.jmk_background
+            else if(tag == KAYVEE) return R.drawable.kayvee_background
+            else if(tag == QUEEN) return R.drawable.queen_background
+            else if(tag == PEACE) return R.drawable.peace_background
+            else if(tag == PERE) return R.drawable.pere_background
+            else if(tag == MARIA) return R.drawable.maria_background
+            else if(tag == JAY_PAUL) return R.drawable.jaypaul_background
+            else if(tag == JACKIE_B) return R.drawable.jackieb_background
+            else if(tag == EMMANUEL) return R.drawable.emmanuel_background
             else return R.drawable.boma_background // bomas image is our default image
         }
     }
